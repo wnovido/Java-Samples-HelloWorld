162534b0eb0df9a5f8f1e0b1b738c595b8b8d992
wilso_000 <wilso_000@Edna_PC>
2014-09-24 06:54:01 -0400

    new commit

---
+++ src/hello/world/app/HelloWorldApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello.world.app;
+
+/**
+ *
+ * @author wilso_000
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+        * The HelloWorldApp class implements an application that simple prints
+        * Hello World to the standard output.
+        */
+        System.out.println("Hello World");
+    }
+    
+}
